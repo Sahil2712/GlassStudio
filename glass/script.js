be0c994252cde0ba2008879f06342a0c92a5d0e2
Sahil2712 <sahilnaik2712@gmail.com>
2020-08-18 01:06:30 +0530

    o

--- glass/script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function () {
-            let header = document.querySelector('div');
-            let windowPosition = window.scrollY > 0;
-            header.classList.toggle('scrolling-active', windowPosition);
-});
